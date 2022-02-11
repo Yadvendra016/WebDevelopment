157a76dfd70c919075bb19e2561c5be2254b11bb
Yadvendra Shukla <yadvendrashukla919@gmail.com>
2022-02-11 22:14:32 +0530

    Update tutorial

---
+++ js/js6.js
@@ -0,0 +1,9 @@
+function greet(name){
+    console.log(name+ " is a good boy");
+}
+let name = "yadvendra";
+let name2 = "shukla";
+let name3 = "pranshu";
+greet(name);
+greet(name2);
+greet(name3);
